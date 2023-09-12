0ee0a5c3241234653088cbdddc126260c83acf65
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2023-09-12 10:20:18 +0530

    Delete practics directory

--- practics/name.r
+++
@@ -1,8 +0,0 @@
-# Program to input your name and find its length
-
-# Input your name from the user
-name <- readline("Enter your name: ")
-
-# Calculate and print the length of the name
-name_length <- nchar(name)
-cat("Your name has", name_length, "characters.\n")
